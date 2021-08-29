caf25155f269e9817b14e20dd49990fb0f6edc82
Lucas Gomes <lucas.gomes@yungas.com.br>
2021-08-29 17:47:19 -0300

    criando modelos

--- mysite/core/models.py
+++ mysite/core/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+GENDERS = (
+      ('M', 'Male'),
+      ('F', 'Female'),
+      ('N', 'Nothing')
+  )
+class Person(models.Model):
+  """ Campo de caracteres """  
+  name = models.CharField()
+  """ Campo para gerar escolhas """
+  gender = models.CharField(max_length=1, choices = GENDERS)
+  """ Campo de caracteres """
+  location = models.CharField()
+  email = models.CharField()
+  telephone = models.CharField()
+  address = models.CharField()
+
+
+class Place(models.Model):
+  address = models.CharField()
+  """ Criar choices """
+  noise_level = models.CharField()
+  """ Criar choices """
+  place_type = models.CharField()
+  """ Criar choices """
+  region_type = models.CharField()
