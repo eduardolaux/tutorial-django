7debd7c6f0ced7e1450a5c0bcbba1c60496fbd96
Lucas Gomes <lucas.gomes@yungas.com.br>
2021-09-05 18:39:01 -0300

    first page

--- mysite/core/views.py
+++ mysite/core/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Person
 
 # Create your views here.
+def homepage(request):
+  """ Home page """
+  persons = Person.objects.all()
+  ctx = {
+    "persons": persons
+  }
+  return render(request, 'homepage.html', context = ctx)
