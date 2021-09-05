361f53a31001706a4f857f630fa0b6fc7309169d
eduardolaux <eduardodalaux@gmail.com>
2021-09-05 16:13:05 -0300

    Modelo Alterado 1-N

--- mysite/core/models.py
+++ mysite/core/models.py
@@ -1,27 +1,41 @@
 from django.db import models
 
+class Person(models.Model):
 GENDERS = (
       ('M', 'Male'),
       ('F', 'Female'),
       ('N', 'Nothing')
   )
-class Person(models.Model):
   """ Campo de caracteres """  
-  name = models.CharField()
+  name = models.CharField(max_length = 100, null = False, blank = False)
   """ Campo para gerar escolhas """
-  gender = models.CharField(max_length=1, choices = GENDERS)
+  gender = models.CharField(max_length=1, choices = GENDERS, blank = False, null = False)
   """ Campo de caracteres """
   location = models.CharField()
   email = models.CharField()
   telephone = models.CharField()
   address = models.CharField()
 
-
+#%%
 class Place(models.Model):
-  address = models.CharField()
+    CLASSE = (
+            ('B', 'Baixo'),
+            ('M', 'Medio'),
+            ('A', 'Alto')
+            )
+    TIPO_LUGAR = (
+            ('C', 'Comercial'),
+            ('U', 'Urbano'),
+            ('R', 'Rural')
+            )
+  person = models.ForeignKey("Person", on_delete = models.CASCADE, related_name = 'NPS lugar')
+  address = models.CharField(max_length = 200, null = False, blank = False)
   """ Criar choices """
-  noise_level = models.CharField()
+  noise_level = models.CharField(max_length = 1, choices = CLASSE, blank = False, null = False)
   """ Criar choices """
-  place_type = models.CharField()
+  place_type = models.CharField(max_length = 1, choices = TIPO_LUGAR, blank = False, null = False)
   """ Criar choices """
   region_type = models.CharField()
+  
+  #%%
+  
