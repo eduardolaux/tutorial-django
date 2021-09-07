f895d0267e05898f8956d3cadebda524cb27bf1a
eduardolaux <eduardodalaux@gmail.com>
2021-09-07 16:51:00 -0300

    acrescentando places em html

--- mysite/core/views.py
+++ mysite/core/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
-from .models import Person
+from .models import Person, Place
 
 # Create your views here.
 def homepage(request):
   """ Home page """
   persons = Person.objects.all()
+  places = Place.objects.all()
+
   ctx = {
-    "persons": persons
+    "persons": persons,
+    "places": places
   }
   return render(request, 'homepage.html', context = ctx)
