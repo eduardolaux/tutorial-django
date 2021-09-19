973fbb966e75a210a45803ea1d359bd79bdada9f
Lucas Gomes <lucas.gomes@yungas.com.br>
2021-09-19 16:25:30 -0300

    configurando estáticos no url

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -17,7 +17,10 @@ from django.contrib import admin
 from django.urls import path
 from core.views import homepage
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', homepage)
-]
+] +  static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
